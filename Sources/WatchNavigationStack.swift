75a26d02173f54289b9a6caf3f950b3c4568323b
Reed Es <reedes@fastmail.com>
2022-12-22 00:11:36 -0700

    ongoing

--- Sources/WatchNavigationStack.swift
+++ Sources/WatchNavigationStack.swift
@@ -27,53 +27,16 @@ struct WatchNavigationStack: View {
 
     // MARK: - Locals
 
-    @StateObject private var router: MyRouter = .init()
-
     // MARK: - Views
 
     var body: some View {
-        NavigationStack(path: $router.path) {
-            rootContent()
-                .environmentObject(router)
-                .environment(\.managedObjectContext, viewContext)
-                .navigationDestination(for: MyRoutes.self) { route in
-                    switch route {
-                    case .settings:
-                        SettingsForm()
-                    case .about:
-                        AboutView()
-//                    case .routines:
-//                        // TODO: is this even necessary?
-//                        RoutineList()
-                    case let .routineDetail(routineURI):
-                        if let routine = Routine.get(viewContext, forURIRepresentation: routineURI) {
-                            WatchRoutineDetail(routine: routine)
-                                .environmentObject(router)
-                                .environment(\.managedObjectContext, viewContext)
-                        } else {
-                            Text("Routine not available to display detail.")
-                        }
-                    case let .exerciseList(routineURI):
-                        if let routine = Routine.get(viewContext, forURIRepresentation: routineURI) {
-                            ExerciseList(routine: routine)
-                                .environmentObject(router)
-                                .environment(\.managedObjectContext, viewContext)
-                        } else {
-                            Text("Routine not available to display exercise list.")
-                        }
-                    case let .exerciseDetail(exerciseURI):
-                        if let exercise = Exercise.get(viewContext, forURIRepresentation: exerciseURI) {
-                            // TODO: Watch version
-                            Text("Watch Exercise Detail \(exercise.wrappedName)")
-//                            ExerciseDetail(exercise: exercise)
-                                .environmentObject(router)
-                                .environment(\.managedObjectContext, viewContext)
-                        } else {
-                            Text("Exercise not available to display detail.")
-                        }
-                    }
-                }
-        }
+        MyNavigationStack(navData: $navData,
+                          routineDetail: routineDetail,
+                          rootContent: rootContent)
+    }
+
+    private func routineDetail(_ routine: Routine) -> AnyView {
+        WatchRoutineDetail(routine: routine).eraseToAnyView()
     }
 }
 
