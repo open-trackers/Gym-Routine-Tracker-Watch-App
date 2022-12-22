b04b2564b896ab45cae2adc0ccfae1f973a135af
Reed Es <reedes@fastmail.com>
2022-12-21 22:42:22 -0700

    ongoing

---
+++ Sources/WatchNavigationStack.swift
@@ -0,0 +1,20 @@
+//
+//  WatchNavigationStack.swift
+//  Gym Routine Tracker Watch App
+//
+//  Created by Reed Esau on 12/21/22.
+//
+
+import SwiftUI
+
+struct WatchNavigationStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WatchNavigationStack_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchNavigationStack()
+    }
+}
---
+++ Sources/WatchRoutineDetail.swift
@@ -0,0 +1,81 @@
+//
+//  WatchRoutineDetail.swift
+//  Gym Routine Tracker
+//
+//  Created by Reed Esau on 12/21/22.
+//
+
+import SwiftUI
+
+import GroutLib
+import GroutUI
+
+struct WatchRoutineDetail: View {
+    @EnvironmentObject private var router: MyRouter
+
+    @SceneStorage("RoutineDetailTab") private var selectedTab: Int = 0
+
+    @ObservedObject var routine: Routine
+
+    var body: some View {
+        RoutineDetail(routine: routine) {
+            TabView(selection: $selectedTab) {
+                Form {
+                    Section("Name") {
+                        TextFieldWithPresets($routine.wrappedName,
+                                             prompt: "Enter routine name",
+                                             color: routineColor,
+                                             presets: routinePresets)
+                    }
+
+                    Section("Image") {
+                        ImageStepper(initialName: routine.imageName, imageNames: systemImageNames) {
+                            routine.imageName = $0
+                        }
+                    }
+                }
+                .tabItem {
+                    Text("Properties")
+                }
+                .tag(0)
+
+                FakeSection("Exercises") {
+                    ExerciseList(routine: routine)
+                    // .environmentObject(router)
+                }
+                .tabItem {
+                    Text("Exercises")
+                }
+                .tag(1)
+            }
+            .tabViewStyle(.page)
+            .navigationTitle {
+                Text("Routine")
+                    .foregroundColor(routineColor)
+            }
+        }
+    }
+
+    // MARK: - Properties
+
+//    private var exerciseCount: Int {
+//        routine.exercises?.count ?? 0
+//    }
+
+    // MARK: - Actions
+
+//    private func exerciseListAction() {
+//        router.path.append(MyRoutes.exerciseList(routine.uriRepresentation))
+//    }
+
+//    private func onDisappearAction() {
+//        logger.debug("Routine Detail, onDisappear")
+//        PersistenceManager.shared.save()
+//    }
+}
+
+// struct WatchRoutineDetail_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RoutineDetail()
+//    }
+// }
