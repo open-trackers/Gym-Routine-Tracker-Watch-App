e0484bba6a820456e6e7c8def9f9fa17d57ed1ec
Reed Es <reedes@users.noreply.github.com>
2023-03-05 22:54:31 -0700

    Reworking/refactoring to share functionality with Daily Calorie Tracker (#17)

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -15,26 +15,42 @@ import GroutLib
 import GroutUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @EnvironmentObject private var manager: CoreDataStack
+
     @SceneStorage("main-routines-nav") private var routinesNavData: Data?
 
     var body: some View {
-        NavStack(name: "main", navData: $routinesNavData) {
+        NavStack(navData: $routinesNavData, destination: destination) {
             RoutineList()
+        }
+        .onContinueUserActivity(startRoutineActivityType) {
+            handleStartRoutineUA(viewContext, $0)
+        }
+    }
+
+    // handle routes for watchOS-specific views here
+    @ViewBuilder
+    private func destination(_ router: GroutRouter, _ route: GroutRoute) -> some View {
+        switch route {
+        default:
+            GroutDestination(route)
+                .environmentObject(router)
+                .environment(\.managedObjectContext, viewContext)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        let ctx = PersistenceManager.getPreviewContainer().viewContext
+        let manager = CoreDataStack.getPreviewStack()
+        let ctx = manager.container.viewContext
         let routine = Routine.create(ctx, userOrder: 0)
         routine.name = "Back & Bicep"
-        let e1 = Exercise.create(ctx, userOrder: 0)
+        let e1 = Exercise.create(ctx, routine: routine, userOrder: 0)
         e1.name = "Lat Pulldown"
-        e1.routine = routine
-        let e2 = Exercise.create(ctx, userOrder: 1)
+        let e2 = Exercise.create(ctx, routine: routine, userOrder: 1)
         e2.name = "Arm Curl"
-        e2.routine = routine
         return ContentView()
             .environment(\.managedObjectContext, ctx)
     }
