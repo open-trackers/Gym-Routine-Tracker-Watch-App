c4dc75a2040514a25e4d2caa6a105879c1d0ddbc
Reed Es <reedes@fastmail.com>
2023-04-09 19:17:01 -0600

    ongoing

---
+++ WidgetGRT/WidgetGRTBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetGRTBundle.swift
+//  WidgetGRT
+//
+//  Created by Reed Esau on 4/9/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetGRTBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetGRT()
+    }
+}
